87a6a53efe9a566c9baf24e0213d2bd7a3422b94
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-02-24 15:17:02 +0000

    add: devcontainer & debug tools for vscode

---
+++ tools/debug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo Starting qemu...
+
+qemu-system-riscv64 \
+    -machine virt \
+    -bios default \
+    -nographic \
+    -serial mon:stdio \
+    --no-reboot \
+    -gdb tcp::12345 -S \
+    -kernel /workspaces/Scarlet/target/riscv64gc-unknown-none-elf/debug/kernel
