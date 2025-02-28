16fbf617ce32532ce35862469520fa52c3f360a4
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-02-28 15:44:30 +0000

    add: get_paddr

--- kernel/src/vm/vmem.rs
+++ kernel/src/vm/vmem.rs
@@ -4,6 +4,23 @@ pub struct VirtualMemoryMap {
     pub vmarea: MemoryArea,
 }
 
+impl VirtualMemoryMap {
+    pub fn new(pmarea: MemoryArea, vmarea: MemoryArea) -> Self {
+        VirtualMemoryMap {
+            pmarea,
+            vmarea,
+        }
+    }
+
+    pub fn get_paddr(&self, vaddr: usize) -> Option<usize> {
+        if self.vmarea.start <= vaddr && vaddr <= self.vmarea.end {
+            Some(self.pmarea.start + (vaddr - self.vmarea.start))
+        } else {
+            None
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct MemoryArea {
     pub start: usize,
