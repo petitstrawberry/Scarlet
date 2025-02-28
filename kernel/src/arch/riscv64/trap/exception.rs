d4ab4fc73b848901762c9d216238557dba6ccb61
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-02-28 15:50:53 +0000

    add: trap for page fault

--- kernel/src/arch/riscv64/trap/exception.rs
+++ kernel/src/arch/riscv64/trap/exception.rs
@@ -1,7 +1,49 @@
+use core::arch::asm;
+use core::panic;
+
 use crate::arch::Arch;
-use crate::println;
-use crate::print;
+use crate::vm::get_kernel_virtual_memory_manager;
 
 pub fn arch_exception_handler(arch: &mut Arch, cause: usize) {
-    loop {}
+    match cause {
+        /* Instruction page fault */
+        12 => {
+            let vaddr = arch.epc as usize;
+            let manager = get_kernel_virtual_memory_manager();
+            match manager.search_memory_map(vaddr) {
+                Some(mmap) => {
+                    match manager.get_root_page_table() {
+                        Some(root_page_table) => {
+                            let paddr = mmap.get_paddr(vaddr).unwrap();
+                            root_page_table.map(vaddr, paddr);
+                        }
+                        None => panic!("Root page table is not found"),
+                    }
+                }
+                None => (),
+            }
+        }
+        /* Load/Store page fault */
+        13 | 15 => {
+            let vaddr;
+            unsafe {
+                asm!("csrr {}, stval", out(reg) vaddr);
+            }
+            let manager = get_kernel_virtual_memory_manager();
+            match manager.search_memory_map(vaddr) {
+                Some(mmap) => {
+                    match manager.get_root_page_table() {
+                        Some(root_page_table) => {
+                            let paddr = mmap.get_paddr(vaddr).unwrap();
+                            root_page_table.map(vaddr, paddr);
+                        }
+                        None => panic!("Root page table is not found"),
+                    }
+                }
+                None => (),
+            }
+        },
+        _ => {
+        }
+    }
 }
