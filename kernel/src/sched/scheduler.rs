2d0996b89bc1d735669503895bafdca04c8beda8
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-03-01 08:14:36 +0000

    update: sched & dispatcher

--- kernel/src/sched/scheduler.rs
+++ kernel/src/sched/scheduler.rs
@@ -5,12 +5,13 @@
 
 extern crate alloc;
 
-use alloc::vec::Vec;
+use alloc::collections::vec_deque::VecDeque;
 use alloc::string::String;
 
-use crate::{arch::instruction::idle, environment::NUM_OF_CPUS, timer::get_kernel_timer};
+use crate::{arch::{enable_interrupt, instruction::idle, Arch}, environment::NUM_OF_CPUS, library::syscall::schedule, task::TaskState, timer::get_kernel_timer};
 
-use super::{dispatcher::Dispatcher, task::Task};
+use super::dispatcher::Dispatcher;
+use crate::task::{Task, TaskType};
 
 
 
@@ -29,7 +30,7 @@ pub fn get_scheduler() -> &'static mut Scheduler {
 }
 
 pub struct Scheduler {
-    task_queue: [Vec<Task>; NUM_OF_CPUS],
+    task_queue: [VecDeque<Task>; NUM_OF_CPUS],
     dispatcher: [Dispatcher; NUM_OF_CPUS],
     interval: u64, /* in microseconds */
 }
@@ -37,39 +38,58 @@ pub struct Scheduler {
 impl Scheduler {
     pub const fn new() -> Self {
         Scheduler {
-            task_queue: [const { Vec::new() }; NUM_OF_CPUS],
+            task_queue: [const { VecDeque::new() }; NUM_OF_CPUS],
             dispatcher: [const { Dispatcher::new() }; NUM_OF_CPUS],
             interval: 1000, /* 1ms */
         }
     }
 
-        }
+    pub fn init_test_tasks(&mut self) {
+        let task1 = Task::new(String::from("Task1"), 1, TaskType::Kernel);
+        let task2 = Task::new(String::from("Task2"), 2, TaskType::Kernel);
+        let task3 = Task::new(String::from("Task3"), 3, TaskType::Kernel);
+
+        self.add_task(task1, 0);
+        self.add_task(task2, 0);
+        self.add_task(task3, 0);
     }
 
     pub fn add_task(&mut self, task: Task, cpu_id: usize) {
-        self.task_queue[cpu_id].push(task);
+        self.task_queue[cpu_id].push_back(task);
     }
 
-    fn run(&mut self) {
+    fn run(&mut self, cpu: &mut Arch) {
         let cpu_id = 0;
-        let task = self.task_queue[cpu_id].pop();
-        match task {
-            Some(t) => self.dispatcher[cpu_id].dispatch(&t),
-            None => {}
+        if let Some(mut t) = self.task_queue[cpu_id].pop_front() {
+            self.dispatcher[cpu_id].dispatch(cpu, &mut t);
+            if t.state != TaskState::Terminated {
+                self.task_queue[cpu_id].push_back(t);
+            }
         }
     }
 
-    pub fn schedule(&mut self) {
-        let cpu_id = 0;
+    pub fn schedule(&mut self, cpu: &mut Arch) {
+        let cpu_id = cpu.get_cpuid();
 
         let timer = get_kernel_timer();
         timer.stop(cpu_id);
         timer.set_interval_us(cpu_id, self.interval);
-        enable_interrupt();
 
         if !self.task_queue[cpu_id].is_empty() {
             timer.start(cpu_id);
-            self.run();
+            self.run(cpu);
+        }
+    }
+
+    pub fn kernel_schedule(&mut self, cpu_id: usize) {
+        let timer = get_kernel_timer();
+        timer.stop(cpu_id);
+        /* Jump to trap handler immediately */
+        timer.set_interval_us(cpu_id, 0);
+        enable_interrupt();
+        
+        if !self.task_queue[cpu_id].is_empty() {
+            timer.start(cpu_id);
         }
         idle();
     }
