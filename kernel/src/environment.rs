e38b8a3cf71662deefbbdd29792c18fbf3789463
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-03-05 14:41:36 +0000

    add: setup_trampoline

--- kernel/src/environment.rs
+++ kernel/src/environment.rs
@@ -1,2 +1,3 @@
-pub const NUM_OF_CPUS: usize = 4;
+pub const NUM_OF_CPUS: usize = 2;
 pub const RISCV_STIMER_FREQ: u64 = 10000000; // 10MHz
+pub const VMMAX: usize = 0xffffffffffffffff;
--- kernel/src/vm/mod.rs
+++ kernel/src/vm/mod.rs
@@ -7,9 +7,15 @@ use manager::VirtualMemoryManager;
 use vmem::MemoryArea;
 use vmem::VirtualMemoryMap;
 
+use crate::arch::get_cpu;
+use crate::arch::set_trap_frame;
+use crate::arch::set_trap_vector;
 use crate::arch::vm::alloc_virtual_address_space;
 use crate::arch::vm::get_page_table;
 use crate::arch::vm::get_root_page_table_idx;
+use crate::environment::VMMAX;
+use crate::println;
+use crate::print;
 
 pub mod manager;
 pub mod vmem;
@@ -17,6 +23,8 @@ pub mod vmem;
 unsafe extern "C" {
     static __KERNEL_SPACE_START: usize;
     static __KERNEL_SPACE_END: usize;
+    static __TRAMPOLINE_START: usize;
+    static __TRAMPOLINE_END: usize;
 }
 
 static mut KERNEL_VM_MANAGER: Option<VirtualMemoryManager> = None;
@@ -35,7 +43,16 @@ pub fn get_kernel_vm_manager() -> &'static mut VirtualMemoryManager {
 }
 
 fn kernel_vm_manager_init() {
-    let mut manager = VirtualMemoryManager::new();
+    let manager = VirtualMemoryManager::new();
+
+    unsafe {
+        KERNEL_VM_MANAGER = Some(manager);
+    }
+}
+
+/* Initialize MMU and enable paging */
+pub fn kernel_vm_init() {
+    let manager = get_kernel_vm_manager();
 
     let asid = alloc_virtual_address_space(); /* Kernel ASID */
     let root_page_table_idx = get_root_page_table_idx(asid).unwrap();
@@ -46,7 +63,7 @@ fn kernel_vm_manager_init() {
     let kernel_start =  unsafe { &__KERNEL_SPACE_START as *const usize as usize };
     let kernel_end = unsafe { &__KERNEL_SPACE_END as *const usize as usize };
 
-    let memmap = VirtualMemoryMap {
+    let kernel_map = VirtualMemoryMap {
         vmarea: MemoryArea {
             start: kernel_start,
             end: kernel_end,
@@ -56,11 +73,11 @@ fn kernel_vm_manager_init() {
             end: kernel_end,
         }
     };
-    manager.add_memory_map(memmap);
+    manager.add_memory_map(kernel_map);
     /* Pre-map the kernel space */
-    root_page_table.map_memory_area(memmap);
+    root_page_table.map_memory_area(kernel_map);
 
-    let devmap = VirtualMemoryMap {
+    let dev_map = VirtualMemoryMap {
         vmarea: MemoryArea {
             start: 0x0,
             end: 0x8000_0000,
@@ -70,17 +87,55 @@ fn kernel_vm_manager_init() {
             end: 0x8000_0000,
         }
     };
-    manager.add_memory_map(devmap);
+    manager.add_memory_map(dev_map);
 
-    unsafe {
-        KERNEL_VM_MANAGER = Some(manager);
-    }
-}
+    setup_trampoline(manager);
 
-/* Initialize MMU and enable paging */
-pub fn kernel_vm_init() {
-    let manager = get_kernel_vm_manager();
-    let root_page_table = manager.get_root_page_table().unwrap();
     root_page_table.switch(manager.get_asid());
+
+
 }
 
+fn setup_trampoline(manager: &mut VirtualMemoryManager) {
+    let trampoline_start = unsafe { &__TRAMPOLINE_START as *const usize as usize };
+    let trampoline_end = unsafe { &__TRAMPOLINE_END as *const usize as usize } - 1;
+    let trampoline_size = trampoline_end - trampoline_start;
+
+    let arch = get_cpu();
+    let trampoline_vaddr_start = VMMAX - trampoline_size;
+    let trampoline_vaddr_end = VMMAX;
+
+    let trap_entry_paddr = arch.get_user_trap_entry_paddr();
+    let trapframe_paddr = arch.get_trapframe_paddr();
+    let trap_entry_offset = trap_entry_paddr - trampoline_start;
+    let trapframe_offset = trapframe_paddr - trampoline_start;
+
+    let trap_entry_vaddr = trampoline_vaddr_start + trap_entry_offset;
+    let trapframe_vaddr = trampoline_vaddr_start + trapframe_offset;
+    
+    println!("Trampoline paddr  : {:#x} - {:#x}", trampoline_start, trampoline_end);
+    println!("Trap entry paddr  : {:#x}", trap_entry_paddr);
+    println!("Trap frame paddr  : {:#x}", trapframe_paddr);
+    println!("Trampoline vaddr  : {:#x} - {:#x}", trampoline_vaddr_start, trampoline_vaddr_end);
+    println!("Trap entry vaddr  : {:#x}", trap_entry_vaddr);
+    println!("Trap frame vaddr  : {:#x}", trapframe_vaddr);
+    
+    let trampoline_map = VirtualMemoryMap {
+        vmarea: MemoryArea {
+            start: trampoline_vaddr_start,
+            end: trampoline_vaddr_end,
+        },
+        pmarea: MemoryArea {
+            start: trampoline_start,
+            end: trampoline_end,
+        }
+    };
+
+    manager.add_memory_map(trampoline_map);
+    /* Pre-map the trampoline space */
+    manager.get_root_page_table().unwrap().map_memory_area(trampoline_map);
+
+    /* Set trap vector and frame for handling exceptions */
+    set_trap_vector(trap_entry_vaddr);
+    set_trap_frame(trapframe_vaddr);
+}
