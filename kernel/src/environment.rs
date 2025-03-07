d200478f68cb6fe17d6bbb83dfee376749d1572b
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-03-07 04:43:04 +0000

    add: STACK_SIZE const

--- kernel/src/environment.rs
+++ kernel/src/environment.rs
@@ -1,3 +1,4 @@
 pub const NUM_OF_CPUS: usize = 2;
 pub const RISCV_STIMER_FREQ: u64 = 10000000; // 10MHz
 pub const VMMAX: usize = 0xffffffffffffffff;
+pub const STACK_SIZE: usize = 0x80000; // 512KB
