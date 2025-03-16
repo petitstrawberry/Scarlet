48a89cd362c34dd1a5c1022fbc575c9bf4f74b6a
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-03-16 13:30:21 +0000

    move: DeviceDriver

--- kernel/src/device/mod.rs
+++ kernel/src/device/mod.rs
@@ -1,6 +1,26 @@
 pub mod platform;
 
+extern crate alloc;
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+
 pub trait Device {
     fn name(&self) -> &'static str;
     fn id(&self) -> usize;
+}
+
+pub static mut DRIVER_TABLE: Vec<Box<dyn DeviceDriver>> = Vec::new();
+
+pub trait DeviceDriver {
+    fn name(&self) -> &'static str;
+    fn match_device(&self, device: &dyn Device) -> bool;
+    fn probe(&self, device: &dyn Device) -> Result<(), &'static str>;
+}
+
+
+#[allow(static_mut_refs)]
+pub fn driver_register(driver: Box<dyn DeviceDriver>) {
+    unsafe {
+        DRIVER_TABLE.push(driver);
+    }
 }
--- kernel/src/device/platform.rs
+++ kernel/src/device/platform.rs
@@ -1,4 +1,4 @@
-use super::Device;
+use super::*;
 
 pub struct PlatformDevice {
     name: &'static str,
@@ -12,5 +12,44 @@ impl Device for PlatformDevice {
 
     fn id(&self) -> usize {
         self.id
+    }
+}
+
+pub struct PlatformDeviceDriver {
+    name: &'static str,
+    match_fn: fn(&dyn Device) -> bool,
+    probe_fn: fn(&dyn Device) -> Result<(), &'static str>,
+}
+
+impl DeviceDriver for PlatformDeviceDriver {
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn match_device(&self, device: &dyn Device) -> bool {
+        (self.match_fn)(device)
+    }
+
+    fn probe(&self, device: &dyn Device) -> Result<(), &'static str> {
+        (self.probe_fn)(device)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    
+    #[test_case]
+    #[allow(static_mut_refs)]
+    fn test_driver_register() {
+        use super::*;
+        let len = unsafe { DRIVER_TABLE.len() };
+        let driver = Box::new(PlatformDeviceDriver {
+            name: "test",
+            match_fn: |_device| _device.name() == "test",
+            probe_fn: |_device| Ok(()),
+        });
+        driver_register(driver);
+        assert_eq!(unsafe { DRIVER_TABLE.len() }, len + 1);
     }
 }
