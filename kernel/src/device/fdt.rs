bd5a619c29a965201567ccfdaf17b3743639fdf9
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-03-21 12:49:24 +0000

    singleton of FdtManager

--- kernel/src/device/fdt.rs
+++ kernel/src/device/fdt.rs
@@ -3,12 +3,15 @@ use fdt::{Fdt, FdtError};
 #[unsafe(link_section = ".data")]
 static mut FDT_ADDR: usize = 0;
 
+static mut MANAGER: FdtManager = FdtManager::new();
+
 pub struct FdtManager<'a> {
     fdt: Option<Fdt<'a>>,
 }
 
 impl<'a> FdtManager<'a> {
-    pub const fn new() -> Self {
+
+    const fn new() -> Self {
         FdtManager {
             fdt: None,
         }
@@ -32,5 +35,15 @@ impl<'a> FdtManager<'a> {
     
     pub fn get_fdt(&self) -> Option<&Fdt<'a>> {
         self.fdt.as_ref()
+    }
+
+    #[allow(static_mut_refs)]
+    pub fn get_mut_manager() -> &'static mut FdtManager<'a> {
+        unsafe { &mut MANAGER }
+    }
+
+    #[allow(static_mut_refs)]
+    pub fn get_manager() -> &'static FdtManager<'a> {
+        unsafe { &MANAGER }
     }
 }
