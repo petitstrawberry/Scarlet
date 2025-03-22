468d1650826c18c5ad2204e7265d38287f20d3ce
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-03-22 13:27:32 +0000

    add: Descriptor init

--- kernel/src/device/virtio/desc.rs
+++ kernel/src/device/virtio/desc.rs
@@ -1,14 +1,50 @@
+use alloc::vec;
 use alloc::vec::Vec;
 
-#[repr(C)]
 pub struct DescriptorTable {
     pub descriptors: Vec<Descriptor>,
+    pub size: usize,
+    pub free_list: Vec<u16>,
+    pub used_list: Vec<u16>,
+    pub free_head: u16,
+}
+
+impl DescriptorTable {
+    pub fn new(size: usize) -> Self {
+        Self {
+            descriptors: vec![Descriptor::default(); size],
+            size,
+            free_list: (0..size as u16).collect(),
+            used_list: Vec::new(),
+            free_head: 0,
+        }
+    }
+
+    pub fn init(&mut self) {
+        for i in 0..self.size {
+            self.descriptors[i].next = (i + 1) as u16;
+        }
+        self.descriptors[self.size - 1].next = 0;
+        self.free_head = 0;
+    }
 }
 
 #[repr(C)]
+#[derive(Clone, Copy)]
 pub struct Descriptor {
     pub addr: u64,
     pub len: u32,
     pub flags: u16,
     pub next: u16,
+}
+
+impl Default for Descriptor {
+    fn default() -> Self {
+        Self {
+            addr: 0,
+            len: 0,
+            flags: 0x1,
+            next: 0,
+        }
+    }
 }
