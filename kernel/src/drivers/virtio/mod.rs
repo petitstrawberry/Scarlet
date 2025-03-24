a5dfb0186114be4aaaf7aaf16d0752e1590d7a92
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-03-24 07:58:54 +0000

    reimpl: virtqueue rings

--- kernel/src/drivers/virtio/mod.rs
+++ kernel/src/drivers/virtio/mod.rs
@@ -5,27 +5,41 @@ pub mod desc;
 use core::{alloc::Layout, mem};
 
 use alloc::alloc::alloc_zeroed;
-use avail::AvailableRing;
+use avail::{AvailableRing, RawAvailableRing};
 use desc::Descriptor;
-use used::{UsedRing, UsedRingEntry};
+use used::{RawUsedRing, RawUsedRingEntry, UsedRing};
+
+// struct RawVirtQueue {
+//     pub desc: [Descriptor; 0], /* Flexible array member */
+//     pub avail: RawAvailableRing,
+//     pub padding: [u8; 0], /* Padding to align the used ring */
+//     pub used: RawUsedRing,
+// }
 
 pub struct VirtQueue<'a> {
     pub desc: &'a mut [Descriptor],
-    pub avail: &'a mut AvailableRing,
-    pub used: &'a mut UsedRing,
+    pub avail: AvailableRing<'a>,
+    pub used: UsedRing<'a>,
 }
 
 impl<'a> VirtQueue<'a> {
     pub fn new(queue_size: usize) -> Self {
         /* Calculate the size of each ring */
         let desc_size = queue_size * mem::size_of::<Descriptor>();
-        let avail_size = mem::size_of::<AvailableRing>() + queue_size * mem::size_of::<u16>();
-        let used_size = mem::size_of::<UsedRing>() + queue_size * mem::size_of::<UsedRingEntry>();
+        let avail_size = mem::size_of::<RawAvailableRing>() + queue_size * mem::size_of::<u16>();
+        let used_size = mem::size_of::<RawUsedRing>() + queue_size * mem::size_of::<RawUsedRingEntry>();
+
+        /* Floor the sum of desc_size, avail_size to the nearest multiple of 4 */
+        let floor_size = (desc_size + avail_size + 3) & !3;
+        /* Align the size to the nearest multiple of 4 */
+        let align_size = (floor_size + 3) & !3;
+        /* Calculate the size of the padding for the used ring */
+        let padding_size = align_size - (desc_size + avail_size);
 
         /* Make layout for the virtqueue */
         /* The size is the sum of the sizes of the descriptor table, available ring, and used ring */
         let layout = Layout::from_size_align(
-            desc_size + avail_size + used_size,
+            desc_size + avail_size + padding_size + used_size,
             mem::align_of::<Descriptor>(),
         )
         .unwrap();
@@ -41,14 +55,14 @@ impl<'a> VirtQueue<'a> {
         let desc = unsafe { core::slice::from_raw_parts_mut(desc_ptr, queue_size) };
 
         /* Create the available ring */
-        let avail_ptr = unsafe { desc_ptr.add(queue_size) as *mut AvailableRing };
-        let avail = unsafe { &mut *avail_ptr };
+        let avail_ptr = unsafe { desc_ptr.add(queue_size) as *mut RawAvailableRing };
+        let avail = unsafe { AvailableRing::new(queue_size, avail_ptr) };
 
         /* Create the used ring */
         let used_ptr = unsafe {
-            (avail_ptr as *mut u8).add(mem::size_of::<AvailableRing>() + queue_size * mem::size_of::<u16>()) as *mut UsedRing
+            (avail_ptr as *mut u8).add(mem::size_of::<RawAvailableRing>() + queue_size * mem::size_of::<u16>() + padding_size) as *mut RawUsedRing
         };
-        let used = unsafe { &mut *used_ptr };
+        let used = unsafe { UsedRing::new(queue_size, used_ptr) };
 
         Self { desc, avail, used }
     }
@@ -64,10 +78,10 @@ impl<'a> VirtQueue<'a> {
             self.desc[i].next = (i as u16 + 1) % self.desc.len() as u16;
         }
 
-        self.avail.flags = 0;
-        self.avail.index = 0;
-        self.used.flags = 0;
-        self.used.index = 0;
+        *(self.avail.flags) = 0;
+        *(self.avail.index) = 0;
+        *(self.used.flags) = 0;
+        *(self.used.index) = 0;
     }
 
     /// Get the raw pointer to the virtqueue
@@ -92,11 +106,20 @@ impl<'a> VirtQueue<'a> {
     /// usize: The size of the virtqueue in bytes.
     pub fn get_raw_size(&self) -> usize {
         let desc_size = self.desc.len() * mem::size_of::<Descriptor>();
-        let avail_size = mem::size_of::<AvailableRing>() + self.desc.len() * mem::size_of::<u16>();
-        let used_size = mem::size_of::<UsedRing>() + self.desc.len() * mem::size_of::<UsedRingEntry>();
+        let avail_size = mem::size_of::<RawAvailableRing>() + self.desc.len() * mem::size_of::<u16>();
+        let used_size = mem::size_of::<RawUsedRing>() + self.desc.len() * mem::size_of::<RawUsedRingEntry>();
 
         desc_size + avail_size + used_size
     }
+}
+
+pub trait VirtioDevice {
+    fn init(&mut self);
+    fn reset(&mut self);
+    fn read32_register(&self, offset: usize) -> u32;
+    fn write32_register(&mut self, offset: usize, value: u32);
+    fn read64_register(&self, offset: usize) -> u64;
+    fn write64_register(&mut self, offset: usize, value: u64);
 }
 
 #[cfg(test)]
@@ -104,18 +127,18 @@ mod tests {
     use super::*;
 
     #[test_case]
-    fn test_virtqueue() {
-        let queue_size = 8;
+    fn test_initialize_virtqueue() {
+        let queue_size = 16;
         let mut virtqueue = VirtQueue::new(queue_size);
         virtqueue.init();
 
         let total_size = mem::size_of::<Descriptor>() * queue_size
-            + mem::size_of::<AvailableRing>() + queue_size * mem::size_of::<u16>()
-            + mem::size_of::<UsedRing>() + queue_size * mem::size_of::<UsedRingEntry>();
+            + mem::size_of::<RawAvailableRing>() + queue_size * mem::size_of::<u16>()
+            + mem::size_of::<RawUsedRing>() + queue_size * mem::size_of::<RawUsedRingEntry>();
 
         assert_eq!(virtqueue.desc.len(), queue_size);
-        assert_eq!(virtqueue.avail.index, 0);
-        assert_eq!(virtqueue.used.index, 0);
+        assert_eq!(*virtqueue.avail.index, 0);
+        assert_eq!(*virtqueue.used.index, 0);
 
         // Check the size of the allocated memory
         let allocated_size = virtqueue.get_raw_size();
@@ -124,6 +147,8 @@ mod tests {
         // Check the next index of each descriptor
         for i in 0..queue_size {
             assert_eq!(virtqueue.desc[i].next, (i as u16 + 1) % queue_size as u16);
+            assert_eq!(virtqueue.avail.ring[i], 0);
+            assert_eq!(virtqueue.used.ring[i].len, 0);
         }
     }
 }
--- kernel/src/drivers/virtio/used.rs
+++ kernel/src/drivers/virtio/used.rs
@@ -1,23 +1,71 @@
-#[repr(C)]
-pub struct UsedRing {
+#[repr(C, align(4))]
+pub struct RawUsedRing {
     pub flags: u16,
     pub index: u16,
-    pub ring: [UsedRingEntry; 0], /* Flexible array member */
+    pub ring: [RawUsedRingEntry; 0], /* Flexible array member */
     pub avail_event: u16,
 }
 
-#[repr(C)]
 #[derive(Clone)]
-pub struct UsedRingEntry {
+#[repr(C)]
+pub struct RawUsedRingEntry {
     pub id: u32,
     pub len: u32,
 }
 
-impl Default for UsedRingEntry {
+impl Default for RawUsedRingEntry {
     fn default() -> Self {
         Self {
             id: 0,
             len: 0,
+        }
+    }
+}
+
+/// Used ring structure
+/// 
+/// This structure is wrapped around the `RawUsedRing` structure.
+/// It provides a safe interface to access the used ring entries.
+pub struct UsedRing<'a> {
+    size: usize,
+    pub flags: &'a mut u16,
+    pub index: &'a mut u16,
+    pub ring: &'a mut [RawUsedRingEntry],
+    pub avail_event: &'a mut u16,
+}
+
+impl<'a> UsedRing<'a> {
+
+    /// Create a new `UsedRing` instance
+    /// 
+    /// This function creates a new `UsedRing` instance from a raw pointer to a `RawUsedRing`.
+    /// 
+    /// # Safety
+    /// 
+    /// This function is unsafe because it dereferences raw pointers and assumes that the memory layout is correct.
+    /// The caller must ensure that the pointer is valid and points to a properly initialized `RawUsedRing`.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `size` - The size of the ring.
+    /// * `ptr` - A raw pointer to a `RawUsedRing`.
+    /// 
+    /// # Returns
+    /// 
+    /// `UsedRing` - A new `UsedRing` instance.
+    pub unsafe fn new(size: usize, ptr: *mut RawUsedRing) -> Self {
+        let flags = unsafe { &mut (*ptr).flags };
+        let index = unsafe { &mut (*ptr).index };
+        let ring_ptr = unsafe { (*ptr).ring.as_mut_ptr() };
+        let ring = unsafe { core::slice::from_raw_parts_mut(ring_ptr, size) };
+        let avail_event = unsafe { &mut *((*ptr).ring.as_mut_ptr().add(size) as *mut u16) };
+
+        Self {
+            size,
+            flags,
+            index,
+            ring,
+            avail_event,
         }
     }
 }
