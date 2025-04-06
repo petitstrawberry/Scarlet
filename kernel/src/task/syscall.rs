8280353ac4ef66d9016a1920e8fef897be596f40
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-04-06 13:51:32 +0000

    fix: update sys_brk and sys_sbrk to use get_arg(0) for argument retrieval

--- kernel/src/task/syscall.rs
+++ kernel/src/task/syscall.rs
@@ -4,7 +4,7 @@ use super::mytask;
 
 pub fn sys_brk(trapframe: &mut Trapframe) -> usize {
     let task = mytask().unwrap();
-    let brk = trapframe.get_arg(1);
+    let brk = trapframe.get_arg(0);
     match task.set_brk(brk) {
         Ok(_) => task.get_brk(),
         Err(_) => usize::MAX, /* -1 */
@@ -13,7 +13,7 @@ pub fn sys_brk(trapframe: &mut Trapframe) -> usize {
 
 pub fn sys_sbrk(trapframe: &mut Trapframe) -> usize {
     let task = mytask().unwrap();
-    let increment = trapframe.get_arg(1);
+    let increment = trapframe.get_arg(0); // Change to get_arg(0)
     let brk = task.get_brk();
     match task.set_brk(unsafe { brk.unchecked_add(increment) }) {
         Ok(_) => brk,
