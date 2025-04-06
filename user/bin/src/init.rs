d1ae6bea38decb4e458718f52b6c0fdd431425b0
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-04-06 13:14:56 +0000

    refactor: rename libscarlet to scarlet_std and implement a bump allocator re-export: core, alloc crate as scarlet_std

--- user/bin/src/init.rs
+++ user/bin/src/init.rs
@@ -1,13 +1,9 @@
 #![no_std]
 #![no_main]
-#![feature(naked_functions)]
-#![feature(used_with_arg)]
 
-extern crate scarlet;
+extern crate scarlet_std as std;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn main() {
-    // This is the main function of the user program.
-    // You can put your code here.
     loop {}
 }
