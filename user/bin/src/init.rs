20ac8e8b4d0d0ee29c306e263ec4a445d5093d44
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-05-06 07:01:20 +0000

    fix: improve logging for child process exit status in init function

--- user/bin/src/init.rs
+++ user/bin/src/init.rs
@@ -24,7 +24,7 @@ pub extern "C" fn main() {
         pid => {
             println!("init: I am the parent process, child PID: {}", pid);
             let res = waitpid(pid, 0);
-            println!("init: Child process exited with status: {}", res.1);
+            println!("init: Child process (PID={}) exited with status: {}", res.0, res.1);
             if res.1 != 0 {
                 println!("init: Child process exited with error");
             }
