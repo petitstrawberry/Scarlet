846829eff598e665a10df1e29c16935803d57ef6
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-07-06 11:35:15 +0000

    feat: Optimize syscall assembly code by removing redundant instructions and using inlateout for better register handling

--- user/lib/src/arch/riscv64.rs
+++ user/lib/src/arch/riscv64.rs
@@ -50,114 +50,94 @@ pub fn _start(a0: usize, a1: usize) -> ! {
 }
 
 pub fn arch_syscall0(syscall: Syscall) -> usize{
-    let mut ret: usize;
+    let mut ret;
     unsafe {
         asm!(
-        "mv a7, {syscall}
-        ecall",
-        syscall = in(reg) syscall as usize,
-        out("a0") ret,
-        options(nostack)
+            "ecall",
+            in("a7") syscall as usize,
+            out("a0") ret,
+            clobber_abi("C"),
+            options(nostack)
         );
     }
     ret
 }
 
 pub fn arch_syscall1(syscall: Syscall, arg1: usize) -> usize{
-    let mut ret: usize;
+    let mut ret;
     unsafe {
         asm!(
-        "mv a7, {syscall}
-        mv a0, {arg1}
-        ecall",
-        syscall = in(reg) syscall as usize,
-        arg1 = in(reg) arg1,
-        out("a0") ret,
-        options(nostack)
+            "ecall",
+            in("a7") syscall as usize,
+            inlateout("a0") arg1 => ret,
+            clobber_abi("C"),
+            options(nostack)
         );
     }
     ret
 }
 
 pub fn arch_syscall2(syscall: Syscall, arg1: usize, arg2: usize) -> usize{
-    let mut ret: usize;
+    let mut ret;
     unsafe {
         asm!(
-        "mv a7, {syscall}
-        mv a0, {arg1}
-        mv a1, {arg2}
-        ecall",
-        syscall = in(reg) syscall as usize,
-        arg1 = in(reg) arg1,
-        arg2 = in(reg) arg2,
-        out("a0") ret,
-        options(nostack)
+            "ecall",
+            in("a7") syscall as usize,
+            inlateout("a0") arg1 => ret,
+            in("a1") arg2,
+            clobber_abi("C"),
+            options(nostack)
         );
     }
     ret
 }
 
-pub fn arch_syscall3(syscall: Syscall, arg1: usize, arg2: usize, arg3: usize) -> usize{
-    let mut ret: usize;
+pub fn arch_syscall3(syscall: Syscall, arg1: usize, arg2: usize, arg3: usize) -> usize {
+    let mut ret;
     unsafe {
         asm!(
-        "mv a7, {syscall}
-        mv a0, {arg1}
-        mv a1, {arg2}
-        mv a2, {arg3}
-        ecall",
-        syscall = in(reg) syscall as usize,
-        arg1 = in(reg) arg1,
-        arg2 = in(reg) arg2,
-        arg3 = in(reg) arg3,
-        out("a0") ret,
-        options(nostack)
+            "ecall",
+            in("a7") syscall as usize,
+            inlateout("a0") arg1 => ret,
+            in("a1") arg2,
+            in("a2") arg3,
+            clobber_abi("C"),
+            options(nostack)
         );
     }
     ret
 }
 
 pub fn arch_syscall4(syscall: Syscall, arg1: usize, arg2: usize, arg3: usize, arg4: usize) -> usize {
-    let mut ret: usize;
+    let mut ret;
     unsafe {
         asm!(
-        "mv a7, {syscall}
-        mv a0, {arg1}
-        mv a1, {arg2}
-        mv a2, {arg3}
-        mv a3, {arg4}
-        ecall",
-        syscall = in(reg) syscall as usize,
-        arg1 = in(reg) arg1,
-        arg2 = in(reg) arg2,
-        arg3 = in(reg) arg3,
-        arg4 = in(reg) arg4,
-        out("a0") ret,
-        options(nostack)
+            "ecall",
+            in("a7") syscall as usize,
+            inlateout("a0") arg1 => ret,
+            in("a1") arg2,
+            in("a2") arg3,
+            in("a3") arg4,
+            clobber_abi("C"),
+            options(nostack)
         );
     }
     ret
 }
 
 pub fn arch_syscall5(syscall: Syscall, arg1: usize, arg2: usize, arg3: usize, arg4: usize, arg5: usize) -> usize {
-    let mut ret: usize;
+    let mut ret;
     unsafe {
         asm!(
-        "mv a7, {syscall}
-        mv a0, {arg1}
-        mv a1, {arg2}
-        mv a2, {arg3}
-        mv a3, {arg4}
-        mv a4, {arg5}
-        ecall",
-        syscall = in(reg) syscall as usize,
-        arg1 = in(reg) arg1,
-        arg2 = in(reg) arg2,
-        arg3 = in(reg) arg3,
-        arg4 = in(reg) arg4,
-        arg5 = in(reg) arg5,
-        out("a0") ret,
-        options(nostack)
+            "ecall",
+            in("a7") syscall as usize,
+            inlateout("a0") arg1 => ret,
+            in("a1") arg2,
+            in("a2") arg3,
+            in("a3") arg4,
+            in("a4") arg5,
+            clobber_abi("C"),
+            options(nostack)
         );
     }
     ret
