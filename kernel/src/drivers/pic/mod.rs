90f1a67bda0ffca3f198dc37d2115ac6d9ae71dc
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-07-07 07:49:52 +0000

    add: implement RISC-V CLINT and PLIC interrupt controllers

--- kernel/src/drivers/pic/mod.rs
+++ kernel/src/drivers/pic/mod.rs
@@ -1,2 +1,10 @@
+//! Platform Interrupt Controller (PIC) implementations
+//!
+//! This module contains implementations of various interrupt controllers
+//! used in different platforms and architectures.
+
 pub mod plic;
 pub mod clint;
+
+pub use plic::Plic;
+pub use clint::Clint;
