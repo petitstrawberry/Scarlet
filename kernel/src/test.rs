01c7faf0b69e242a5c34799a18003ce961635bff
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-07-09 09:17:00 +0000

    fix: replace println! with early_println! in panic handler and test runner

--- kernel/src/test.rs
+++ kernel/src/test.rs
@@ -1,28 +1,25 @@
 use core::panic::PanicInfo;
 
 use crate::arch;
-use crate::println;
+use crate::early_println;
 
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("[Scarlet Kernel] panic: {}", info);
-    println!("[Test Runner] Test failed");
-    
+    early_println!("[Scarlet Kernel] panic: {}", info);
+    early_println!("[Test Runner] Test failed");
+
     arch::shutdown();
 }
 
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
 
-    use crate::println;
-
-    println!("[Test Runner] Running {} tests", tests.len());
+    early_println!("[Test Runner] Running {} tests", tests.len());
     for test in tests {
-        // println!("[Test Runner] Running test: {:?}", test as *const _);
         test();
     }
 
-    println!("[Test Runner] All tests passed");
+    early_println!("[Test Runner] All tests passed");
     arch::shutdown();
 }
