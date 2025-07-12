e0e50cea2df7228ab47ebb903473e4666c49286c
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-07-12 15:34:54 +0000

    feat: add create_directory function to allow creation of new directories

--- user/lib/src/fs.rs
+++ user/lib/src/fs.rs
@@ -767,6 +767,32 @@ pub fn pivot_root(new_root: &str, old_root: &str) -> Result<()> {
     }
 }
 
+/// Create a new directory
+/// 
+/// This function creates a new directory at the specified path.
+/// 
+/// # Arguments
+/// * `path` - Path to the new directory
+/// 
+pub fn create_directory<P: AsRef<str>>(path: P) -> Result<()> {
+    use crate::syscall::{syscall1, Syscall};
+    use crate::ffi::str_to_cstr_bytes;
+
+    let path_c = str_to_cstr_bytes(path.as_ref())
+        .map_err(|_| Error::new(ErrorKind::InvalidInput, "path contains null byte"))?;
+
+    let result = syscall1(
+        Syscall::VfsCreateDirectory,
+        path_c.as_ptr() as usize,
+    );
+
+    if result == usize::MAX {
+        Err(Error::new(ErrorKind::Other, "create directory failed"))
+    } else {
+        Ok(())
+    }
+}
+
 /// Change the current working directory
 ///
 /// # Arguments
