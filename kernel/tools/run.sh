833c7b5f44ef0730924c28451cbb989f6b45c77a
petitstrawberry <petitstrawberry.dev@gmail.com>
2025-07-15 02:36:18 +0000

    qemu: specify bus for virtio block and GPU devices

--- kernel/tools/run.sh
+++ kernel/tools/run.sh
@@ -20,8 +20,8 @@ qemu-system-riscv64 \
     --no-reboot \
     -global virtio-mmio.force-legacy=false \
     -drive id=x0,file=test.txt,format=raw,if=none \
-    -device virtio-blk-device,drive=x0 \
-    -device virtio-gpu-device \
+    -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0 \
+    -device virtio-gpu-device,bus=virtio-mmio-bus.1 \
     -vnc :0 \
     -initrd "$INITRAMFS_PATH" \
     -kernel $1 | tee "$TEMP_OUTPUT"
