3e765ce73b44c4b73774974c779a3c32c7987904
petitstrawberry <petitstrawberry.dev@gmail.com>
2026-03-22 15:30:30 +0000

    drivers: remove architecture-specific conditionals for device modules

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -3,27 +3,22 @@
 //! This module encapsulates various device drivers for the Scarlet kernel.
 
 pub mod block;
-#[cfg(target_arch = "aarch64")]
 pub mod display;
 pub mod graphics;
-#[cfg(target_arch = "aarch64")]
 pub mod i2c;
-#[cfg(target_arch = "aarch64")]
 pub mod iommu;
 pub mod network;
-#[cfg(target_arch = "aarch64")]
 pub mod nvmem;
-#[cfg(target_arch = "aarch64")]
 pub mod pcie;
-#[cfg(target_arch = "aarch64")]
 pub mod phy;
 pub mod pic;
-#[cfg(target_arch = "aarch64")]
 pub mod soc;
 pub mod special;
+pub mod spi;
 pub mod uart;
 pub mod usb;
 pub mod virtio;
 pub mod virtio_input;
 pub mod virtio_rng;
 pub mod watchdog;
+pub mod gpio;
