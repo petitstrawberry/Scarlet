d0f669ae24e3c68de643d3ed3f800d42d6f1a2b1
petitstrawberry <petitstrawberry.dev@gmail.com>
2026-03-26 11:09:56 +0000

    refactor: add scarlet dependency to module prototype and update initialization

--- modules/scarlet-module-prototype/src/lib.rs
+++ modules/scarlet-module-prototype/src/lib.rs
@@ -1,5 +1,11 @@
 #![no_std]
 
+fn register_prototype() {
+    scarlet::early_println!("[scarlet-module-prototype] Hello from external module!");
+}
+
+scarlet::driver_initcall!(register_prototype);
+
 #[used]
 static SCARLET_MODULE_PROTOTYPE_ANCHOR: fn() = force_link;
 
